7ec3865fc4673f3c2e518e00ed325e4cbc1cde38
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-07-11 22:09:39 -0300

    add migrations

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'categoria',
-    'produto'
+    'core.categoria',
+    'core.produto'
 ]
 
 MIDDLEWARE = [
