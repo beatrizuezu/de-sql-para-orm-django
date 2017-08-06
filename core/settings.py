7da68f34b48eabb31e7fe2bc2e8ad74a33eb5390
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-08-06 13:43:34 -0300

    rename apps

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.categoria',
-    'core.produto'
+    'core.categorias',
+    'core.produtos'
 ]
 
 MIDDLEWARE = [
