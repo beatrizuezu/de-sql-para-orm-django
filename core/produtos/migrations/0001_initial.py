9abf8383bf510bc54907e66a6512099664df4cf4
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-08-06 13:58:11 -0300

    rename app name in  migration

--- core/produtos/migrations/0001_initial.py
+++ core/produtos/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('categoria', '0001_initial'),
+        ('categorias', '0001_initial'),
     ]
 
     operations = [
