ff430142ad455a6896f5714d5d77308716d62e12
Beatriz Uezu <beatriz.uezu@gmail.com>
2021-07-01 23:12:08 -0300

    fix app categorias

--- core/categorias/apps.py
+++ core/categorias/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CategoriaConfig(AppConfig):
-    name = 'categoria'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core.categorias'
